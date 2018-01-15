5eaa2ed109daf0cdeb7a34783f919c41ce54be94
yukihirop <te108186@gmail.com>
2018-01-16 01:16:27 +0900

    bundle exec annotate

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: revisions
+#
+#  id         :integer          not null, primary key
+#  post_id    :integer          not null
+#  user_id    :integer          not null
+#  title      :text             not null
+#  summary    :text             not null
+#  goal       :text             not null
+#  comment    :text
+#  event_url  :text
+#  body       :text             not null
+#  slide_url  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Revision < ApplicationRecord
   belongs_to :post
   belongs_to :user
