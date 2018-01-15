516d8f4f23a3428d3e1213e3e4e333bd93808a5a
yukihirop <te108186@gmail.com>
2018-01-15 22:58:46 +0900

    bin/rails db:migrate

--- db/migrate/20180115134929_create_posts.rb
+++ db/migrate/20180115134929_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-      t.references :user, foreign_key: true
-      t.integer :newest_revision_id
+      t.references :user, foreign_key: true, null: false
+      t.integer :newest_revision_id, null: false
 
       t.timestamps
     end
