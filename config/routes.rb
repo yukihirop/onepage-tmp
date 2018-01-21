d94b7b8e3eca0bc93a6d441c17799ae54ec8e353
yukihirop <te108186@gmail.com>
2018-01-22 01:13:51 +0900

    WelcomePageを表示する(Rails側)
    - 該当しないパスはapplication.html.erbをレンダリングするようにする。
    - application.html.erbには「application」のidを持つbodyを用意する
    
    [参考]
    https://github.com/kami-zh/repost

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '*path', to: 'application#root'
+
+  root 'application#root'
 end
